69bd175ccbcad26b7a0c5b18cc5190c1d9344ccd
Zorro Liu <lyx@rock-chips.com>
2025-09-03 17:18:51 +0800

    drm/rockchip: ebc_dev: release version v8.10
    Change-Id: I633801ba1f173cff2828fc26098c374aa824fe1a
    Signed-off-by: Zorro Liu <lyx@rock-chips.com>

--- drivers/gpu/drm/rockchip/ebc-dev/epdlut/epd_lut.c
+++ drivers/gpu/drm/rockchip/ebc-dev/epdlut/epd_lut.c
@@ -96,3 +96,17 @@ int epd_overlay_lut(void)
 {
 	return WF_TYPE_GRAY2;
 }
+
+//return value
+//0 : no modify  1: modify by customer
+int epd_gray2_last_repair(u8 *wf_table)
+{
+	return 0;
+}
+
+//return value
+//0 : no modify  1: modify by customer
+int epd_overlay_gray2_repair(u8 *wf_table)
+{
+	return 0;
+}
